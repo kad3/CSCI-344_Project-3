c7059ae2f7ba1eb0163725c96e47b7b63674567a
Kirk Daniels <kdaniels@unca.edu>
2012-04-04 21:16:39 -0400

    pops old links

--- twitter.js
+++ twitter.js
@@ -15,17 +15,29 @@ t.stream(
     'statuses/filter',
     { track: ['awesome','linux'] },
     function(stream) {
+        var awesome = 0;
+        var linux = 0;
         stream.on('data', function(tweet) {
             if(tweet.text.match(/awesome/)) {
                if(tweet.entities.urls[0]) {
                  console.log(tweet.entities.urls[0].expanded_url);
                  client.lpush('links',tweet.entities.urls[0].expanded_url);
+                 awesome = awesome + 1;
+                 if (awesome > 10) {
+                   client.rpop('links');
+                   console.log("POP:awesome");
+                 }
                }
             }
             if(tweet.text.match(/linux/)) {
                if(tweet.entities.urls[0]) {
                  console.log(tweet.entities.urls[0].expanded_url);
                  client.lpush('linux',tweet.entities.urls[0].expanded_url);
+                 linux = linux + 1;
+                 if (linux > 10){
+                   client.rpop('linux');
+                   console.log("POP:linux");
+                 }
                }
             }
         });
